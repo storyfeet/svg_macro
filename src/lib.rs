7c49baf5d7c7976b37e9a6b77592f6b56264174d
storyfeet <thebeaky@psmail.net>
2019-05-07 14:47:01 +0100

    init

---
+++ src/lib.rs
@@ -0,0 +1,82 @@
+
+
+#[macro_export]
+macro_rules! svg{
+    ($w:expr,$h:expr=>$e:tt)=>(svg_branch!({
+        svg width=$w, height=$h,
+        xmlns="http://www.w3.org/2000/svg"
+        //xmlns:xlink= "http://www.w3.org/1999/xlink"
+            => $e
+    }));
+}
+
+
+#[macro_export]
+macro_rules! svg_list{
+    ({$($t:tt),*})=>({
+        let mut s = String::new();
+        $(
+            s.push_str(&svg_branch!($t));
+        )*
+        s
+    });
+    (($i:ident))=>(stringify!($i));
+    (($s:expr))=>(stringify!($s));
+     
+}
+
+#[macro_export]
+macro_rules! svg_property{
+    ({ }) => {""};
+    (($e:expr)) => {$e};
+    ($e:expr)=>{stringify!($e)}
+}
+
+#[macro_export]
+macro_rules! svg_branch {
+    ($l:literal)=>{stringify!($l)};
+    ({$n:ident $($p:ident=$v:tt),* => $e:tt}) => {
+        {
+        let mut props = String::new();
+        $(
+        props.push_str(&format!("{}=\"{}\"",stringify!($p),svg_property!($v)));
+        )*
+        format!("<{0} {1}>{2}</{0}>",stringify!($n), props,svg_list!($e))
+        }
+    };
+    ({$n:ident $($p:ident=$v:expr),*;})=>{
+        {
+        let mut props = String::new();
+        $(
+        props.push_str(&format!("{}=\"{}\"",stringify!($p),$v));
+        )*
+        format!("<{} {}/>",stringify!($n),props)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn top_level(){
+        assert_eq!(
+            &svg!{
+                5,7=>{
+                    {big x=4,y=5 ;}
+                }
+            }, 
+            "<svg width=\"5\"height=\"7\"xmlns=\"\"http://www.w3.org/2000/svg\"\"></svg>")
+    }
+    #[test]
+    fn it_works() {
+        assert_eq!(&svg_branch!({big x=4,y=(4+5) ;}), "<big>5</big>");
+    }
+    #[test]
+    fn test_text() {
+        assert_eq!(
+            &svg_branch!({text x=4 => (hello)}),
+            "<text x=\"4\">hello</text>"
+            );
+    }
+}
